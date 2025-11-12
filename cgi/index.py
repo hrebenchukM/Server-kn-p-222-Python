10876003872fe23979cbbffe0511f4cbb7246d5c
Maryprog <marya101204@gmail.com>
2025-11-13 01:52:44 +0200

    hw table

--- cgi/index.py
+++ cgi/index.py
@@ -6,14 +6,36 @@ sys.stdin = codecs.getreader("utf-8")(sys.stdin.detach())
 
 # параметри від сервера передаються через змінні оточення,
 # які доступні через пакет os
-envs = "<ul>" + "\n".join(f"<li>{k} = {v}</li>" for k,v in os.environ.items()) + "</ul>"
+
+rows = "\n".join(
+    f"<tr><td>{k}</td><td>{v}</td></tr>"
+    for k, v in sorted(os.environ.items(), key=lambda item: item[0].lower())
+)
+
+
+
+envs = f"""
+<table border="1" cellpadding="6" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Змінна</th>
+            <th>Значення</th>
+        </tr>
+    </thead>
+    <tbody>
+        {rows}
+    </tbody>
+</table>
+"""
+
+# envs = "<ul>" + "\n".join(f"<li>{k} = {v}</li>" for k,v in os.environ.items()) + "</ul>"
 
 html = f'''<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CGI-2222</title>
+    <title>CGI-222</title>
     <link rel="icon" type="image/png" href="python.png" />
 </head>
 <body>
