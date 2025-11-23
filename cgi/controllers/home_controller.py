949d6be2fc5b48a8e6e60347e9263dff673ca508
Maryprog <marya101204@gmail.com>
2025-11-23 08:40:50 +0200

    /hw

--- cgi/controllers/home_controller.py
+++ cgi/controllers/home_controller.py
@@ -43,7 +43,7 @@ class HomeController :
                     <link rel="stylesheet" href="/css/site.css" />
             </head>
             <body>
-                    {{RenderBody}}
+                   RenderBody
                     <script src="/js/site.js"></script>
             </body>
             </html>'''
@@ -55,20 +55,73 @@ class HomeController :
         qp   = "<ul>" + "".join(f"<li>{k} = {v}</li>" for k,v in self.cgi_request.query.items()  ) + "</ul>"
         hdrs = "<ul>" + "".join(f"<li>{k} = {v}</li>" for k,v in self.cgi_request.headers.items()) + "</ul>"
 
-        html =self._layout().replace(
-            "RenderBody", '''
-            <h1>Інтерфейс спільного шлюзу (CGI)</h1>
-            <a href = "/home/privacy">Політика конфеденційності</a>
-            {envs}
-            {qp}
-            {hdrs}
-            <img src="/img/m13.jpg" />
-            <script src="/js/site.js"></script>
-            '''
-        ) 
+        html = self._layout().replace(
+        "RenderBody", f'''
+        <h1>Інтерфейс спільного шлюзу (CGI)</h1>
+        <a href = "/home/privacy">Політика конфеденційності</a>
+
+        <h2>ENV</h2>
+        {envs}
+
+        <h2>QUERY</h2>
+        {qp}
+
+        <h2>HEADERS</h2>
+        {hdrs}
+
+        <img src="/img/m13.jpg" />
+        <hr>
+
+        <h2>Зміст</h2>
+        <ul>
+            <li><a href="/home/params">Параметри CGI (ENV, QUERY, HEADERS)</a></li>
+            <li><a href="/home/privacy">Політика конфіденційності</a></li>
+
+            
+            <li><a href="/wantsleep/index">Неправильний контролер</a></li>
+            <li><a href="/home/wantsleep">Неправильний метод</a></li>
+        </ul>
+
+        <script src="/js/site.js"></script>
+        '''
+    )
+
         
 
         print("Content-Type: text/html; charset=utf-8")
-        print()   # порожній рядок відділяє заголовки та тіло
+        print()  
         print(html)
+
+    def params(self):
+
+        def table(d: dict):
+            rows = "".join(f"<tr><td>{k}</td><td>{v}</td></tr>" for k,v in d.items())
+            return f"<table border='1' cellpadding='4'>{rows}</table>"
+
+        envs = table(self.cgi_request.server)
+        qp   = table(self.cgi_request.query)
+        hdrs = table(self.cgi_request.headers)
+
+        html = self._layout().replace(
+            "RenderBody", f'''
+            <h1>Параметри CGI</h1>
+
+            <h2>Змінні оточення (ENV)</h2>
+            {envs}
+
+            <h2>Параметри запиту (QUERY)</h2>
+            {qp}
+
+            <h2>Заголовки (HEADERS)</h2>
+            {hdrs}
+
+            <a href="/home/index">⬅ назад</a>
+            '''
+        )
+
+        print("Content-Type: text/html; charset=utf-8")
+        print()
+        print(html)
+
+
 
